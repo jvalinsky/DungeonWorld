31fcafe2ce5301fba626a59817cc05a8af5b2a8f
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-04-19 19:21:27 -0400

    refactoring def-room

--- gridworld-dungeonworld.lisp
+++ gridworld-dungeonworld.lisp
@@ -1,7 +1,19 @@
 ; returns a list of points for a room
 ; symbol format ?room-?x-?y
-(defun def-room (?room ?x ?y)
-  (loop for x from 0 to ?x
-        append
-            (loop for y from 0 to ?y
-                collect (intern (format nil "~{~a~^-~}" (list ?room x y))))))
+
+(defun room-pts-rect (?room ?x1 ?x2 ?y1 ?y2)
+  (loop for x from ?x1 to ?x2
+                     append
+                     (loop for y from ?y1 to ?y2
+                           collect (intern (format nil "~{~a~^-~}" (list ?room x y))))))
+
+; for loop in for loop that checks if there should be a path between points
+; then makes path
+(defun room-edges-rect (?room ?points)
+
+    )
+
+(defun def-room (?room ?x1 ?x2 ?y1 ?y2)
+  (let* ((points (room-pts-rect ?room ?x1 ?x2 ?y1 ?y2))
+        (edges (room-edges-rect ?room points)))
+    (list points edges)))
