da6df6e0a0da7c842d9e55fca5ddfd3bb6994f4c
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-04-19 19:38:01 -0400

    started making map

--- gridworld-dungeonworld.lisp
+++ gridworld-dungeonworld.lisp
@@ -1,6 +1,8 @@
 ; returns a list of points for a room
-; symbol format ?room-?x-?y
+; HELPER FUNCTIONS FOR BUILDING MAP
+; ====================================================================================================
 
+; symbol format ?room-?x-?y
 (defun room-pts-rect (?room ?x1 ?x2 ?y1 ?y2)
   (loop for x from ?x1 to ?x2
                      append
@@ -17,3 +19,26 @@
   (let* ((points (room-pts-rect ?room ?x1 ?x2 ?y1 ?y2))
         (edges (room-edges-rect ?room points)))
     (list points edges)))
+
+; ===================================================================================================
+
+(defvar *dungeonworld-points* '())
+(defvar *dungeonworld-paths* '())
+
+(let* ((main (def-room 'main 0 10 0 10))
+       (main-points (car main))
+       (main-paths (car (car main))))
+  (append dungeonworld-points main-points)
+  (append dungeonworld-paths main-paths))
+
+
+(def-roadmap *dungeonworld-points* *dungeonworld-paths*)
+
+(def-object 'apple '(is_inanimate is_edible (has_cost 3.0)))
+
+(place-object 'apple1-main 'apple 'main-5-5 0 
+	nil 
+	'((is_edible apple1-main) 
+	 )
+    nil 
+)
