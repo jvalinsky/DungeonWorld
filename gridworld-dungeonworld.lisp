f3ecedf6cdabfb2d6ea04c0bff6ac22a8337a7dd
River <jkjk822@yahoo.com.au>
2017-04-24 14:07:14 -0400

    Item/container can't be on top of AG

--- gridworld-dungeonworld.lisp
+++ gridworld-dungeonworld.lisp
@@ -272,8 +272,7 @@
 
 ;Helper to see if two points are adjacent
 (defun is_adjacent? (?x ?y)
-  ;;(format t "~S is_adjacent? ~S~%" ?x ?y)
-  (not (null (find
+  (equal 1
     (+
       (abs
         (-
@@ -283,55 +282,52 @@
         (-
           (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
           (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y))))))))
-    '(0 1))))
-)
+))
 
 ;Helper to test that ?x is ?dir of ?y (e.g. x is NORTH of y or similar)
 (defun is_direction? (?dir ?x ?y)
-  (if (equal ?x ?y)
-    t
-    (cond 
-      ((equal ?dir 'EAST)
-        (and
-          (equal 0
-            (-
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
-          (< 0
-            (-
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
-      ((equal ?dir 'WEST)
-        (and
-          (equal 0
-            (-
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
-          (> 0
-            (-
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
-      ((equal ?dir 'NORTH)
-        (and
-          (< 0
-            (-
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
-          (equal 0
-            (-
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
-      ((equal ?dir 'SOUTH)
-        (and
-          (> 0
-            (-
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
-                (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
-          (equal 0
-            (-
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
-                (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y))))))))))
-  ))
+  (cond 
+    ((equal ?dir 'EAST)
+      (and
+        (equal 0
+          (-
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
+        (< 0
+          (-
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
+    ((equal ?dir 'WEST)
+      (and
+        (equal 0
+          (-
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
+        (> 0
+          (-
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
+    ((equal ?dir 'NORTH)
+      (and
+        (< 0
+          (-
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
+        (equal 0
+          (-
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y)))))))))
+    ((equal ?dir 'SOUTH)
+      (and
+        (> 0
+          (-
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?x))))
+              (parse-integer (cadr (split-regexp "&" (symbol-name ?y))))))
+        (equal 0
+          (-
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?x)))))
+              (parse-integer (cadr (cdr (split-regexp "&" (symbol-name ?y))))))))))
+  )
 
 (defun checkKey? (?item)
   (if (equal (subseq  (string ?item) 0 3) "key")
